0a2b37485af5c8956629cd442cf6b57e1f106db5
mdalihasanriyad <you@ahriyad.com@gmail.com>
2025-05-20 22:56:37 +0600

    load

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -7,7 +7,7 @@ const Loading = () => {
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowLoad(false); // hide loader after 1s
-            setText('I wanted 3 sec loading');
+            setText('I wanted 1 sec loading');
         }, 1000);
 
         return () => clearTimeout(timer); // cleanup
@@ -16,15 +16,15 @@ const Loading = () => {
     if (showLoad) {
         return (
             <div className="flex items-center justify-center h-screen bg-[#1e1c18]">
-                <div className="relative w-20 h-20">
+                <div className="relative w-24 h-24">
                     {/* Outer ring - white */}
                     <div className="absolute inset-0 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
 
                     {/* Middle ring - yellow */}
-                    <div className="absolute top-2 left-2 w-16 h-16 border-4 border-b-transparent border-yellow-400 rounded-full animate-[spin_3s_linear_infinite]"></div>
+                    <div className="absolute top-2 left-2 w-20 h-20 border-4 border-b-transparent border-yellow-400 rounded-full animate-[spin_3s_linear_infinite]"></div>
 
                     {/* Inner ring - white */}
-                    <div className="absolute top-4 left-4 w-12 h-12 border-2 border-t-transparent border-white rounded-full animate-[spin_2s_linear_infinite_reverse]"></div>
+                    <div className="absolute top-4 left-4 w-16 h-16 border-2 border-t-transparent border-white rounded-full animate-[spin_2s_linear_infinite_reverse]"></div>
                 </div>
             </div>
         );
